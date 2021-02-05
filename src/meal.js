5db44219bdff0150f0eace6802df9a7e10a5985f
Marceline Ball <marchettiball@gmail.com>
2021-02-05 08:50:21 -0700

    Add test and first iteration

--- src/meal.js
+++ src/meal.js
@@ -1,15 +1,65 @@
-function nameMenuItem() {
+function nameMenuItem(food) {
+  return `Delicious ${food}`;
+};
+
+function createMenuItem(food, price, type){
+  return {
+    name: food,
+    price: price,
+    type: type,
+  };
+};
+
+function addIngredients(newIngredient, arrayForIgredients) {
+  if (arrayForIgredients.includes(newIngredient) === false) {
+    arrayForIgredients = arrayForIgredients.push(newIngredient);
+  };
+};
+
+// function addIngredients(newIngredient, arrayForIgredients) {
+//   if (newIngredient !== arrayForIgredients[1]) {
+//     arrayForIgredients = arrayForIgredients.push(newIngredient);
+//   }
+// };
+// This only works for this particular instance of unique ingredients, I need to refactor for others
+
+function formatPrice(itemPrice) {
+  return `$${itemPrice}`;
+};
+
+function decreasePrice(price) {
+  return price = (price * 0.9)
+}
+
+function createRecipe(name, ingredients, menuItemType) {
+  return {
+    title: name,
+    ingredients: ingredients,
+    type: menuItemType,
+  }
 
 }
 
+
+// var ingredients = [];
+// addIngredients("eggs", ingredients);
+// addIngredients("bacon", ingredients);
+//
+// var title = nameMenuItem("Eggs & Bacon");
+// var price = formatPrice("10.85")
+// var menuItem = createMenuItem(title, price, "breakfast");
+// var menuItemType = menuItem.type;
+//
+// var recipe = createRecipe(title, ingredients, menuItemType);
+// assert.equal(recipe.title, "Delicious Eggs & Bacon");
+// assert.deepEqual(recipe.ingredients, ["eggs", "bacon"]);
+// assert.equal(recipe.type, "breakfast");
 
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
-  // formatPrice,
-  // decreasePrice,
-  // createRecipe
+  createMenuItem,
+  addIngredients,
+  formatPrice,
+  decreasePrice,
+  createRecipe
 }
-
-
--- src/warm-up.js
+++ src/warm-up.js
@@ -12,27 +12,29 @@
 // var food = "Ice Cream";
 // var type = "Dessert";
 // var inStock = 10
+var food = 'Croissant';
+var type = 'Pastry';
+var inStock = 4;
 
-
-
-// Reassign (not initialize) your "inStock" variable from above. We got a new shipment! Use a built in math operator to make 
+// Reassign (not initialize) your "inStock" variable from above. We got a new shipment! Use a built in math operator to make
 // the inStock count double what it currently is.
-
-
-// Reassign (not initialize) your "inStock" variable from above. You got hungry and needed a snack! 
+inStock = inStock * 2
+console.log(inStock);
+// Reassign (not initialize) your "inStock" variable from above. You got hungry and needed a snack!
 // Use a built in math operator to decrement the inStock count by 1.
-
-
+inStock = inStock - 1
+console.log(inStock);
 // Initialize a new variable "isFavorite", and assign it to a boolean value indicating
 // if the food you chose is your favorite food or not
-
+var isFavorite = false
 
 
 // Create a new variable named "shouldOrderMore", and use a comparison operator to
 // assign a boolean value to this variable. If "inStock" is less than 20, the
 // variable should be true, otherwise it should be false. You MUST use a
 // comparison operator.
-
+var shouldOrderMore = inStock < 20;
+console.log(shouldOrderMore);
 
 
 //------------------------- Conditionals -------------------------//
@@ -40,43 +42,62 @@
 
 
 // Express the following in code: If the variable "stockCount" is greater or equal to 1, log
-// the statement 'I'm going to eat some [food]!', otherwise, log the statement 
+// the statement 'I'm going to eat some [food]!', otherwise, log the statement
 // 'We need to get some more [food].'
-
-
+var stockCount = 1
+function checkFood(){
+  if (stockCount >= 1) {
+    console.log(`I'm going to eat some ${food}`);
+  } else {
+    console.log(`We need to get some more ${food}`);
+  }
+}
+checkFood();
 
 // Express the following in code: If the variable "isFavorite" is true,
 // log the statement 'My favorite food is [food]!'. Otherwise, log the
 // statement 'I recently ate [food]'. In either case, the value stored in
 // the "food" variable should be included in the logged statement.
-
-
+function checkIfFoodIsFavorite() {
+  if (isFavorite === true) {
+    console.log(`My favorite food is ${food}`);
+  } else {
+    console.log(`I recently ate ${food}`);
+  }
+}
+checkIfFoodIsFavorite();
 
 //------------------------- Arrays -------------------------//
 
 
 // Create a new variable "vegetables" and assign it to an Array of 5 different
 // vegetables, represented by their names as Strings.
-
+var vegetables = ['carrot','potato', 'lettuce', 'cucumber', 'daikon'];
 
 // Write the code to access the 3rd element of the Array, using bracket
 // notation.
-
+console.log(vegetables[2]);
 
 // Create a new variable "fruits" and assign it to an Array of 7
 // different fruits.
-
+var fruits = ['apple', 'pear', 'mango', 'strawberry', 'blueberry', 'avacado', 'banana'];
 
 // Write the code to access the 5th element of the array, using bracket notation.
-
+console.log(fruits[4]);
 
 //------------------------- Objects -------------------------//
 
 // Create a new variable called "groceryStore" and assign to it an object literal with two
 // keys: name, and isOpen. The value for name should the string 'King Soopers' (or your favorite grocery store), and
 // the value for isOpen should be true. Which other key value pairs would make sense to add to this object?
-
+var groceryStore = {
+  name: 'King Soopers',
+  isOpen: true,
+  hoursOpen: '5 AM - 12 AM',
+};
 
 
 // Write the code to add the key of location, with the value of 'Denver, Colorado'. Use dot
 // notation to do this. DO NOT modify your code from above.
+groceryStore.location = 'Denver, Colorado'
+console.log(groceryStore);
