5abfe71f60cb37424d9b9ef6b56628ab377169c4
Marceline Ball <marchettiball@gmail.com>
2021-02-06 19:49:06 -0700

    Add second iteration

--- src/order.js
+++ src/order.js
@@ -1,7 +1,41 @@
+function takeOrder(newOrder, orders) {
+  if (orders.length < 3) {
+    orders = orders.push(newOrder);
+  };
+};
+
+function refundOrder(orderToRefund, totalOrders) {
+  totalOrders = totalOrders.splice((orderToRefund - 1), 1);
+};
+
+function listItems(items) {
+  var itemGroup = ''
+  var itemLength = items.length
+  for (var i = 0; i < (items.length); i++) {
+    if (i < (items.length -1)) {
+      itemGroup += (items[i].item + ', ');
+    } else {
+      itemGroup += (items[i].item);
+    };
+  };
+  return itemGroup;
+};
+
+function searchOrder(orders, searchItem) {
+  for (var i = 0; i < orders.length; i++) {
+    if (orders[i].item.includes(searchItem)) {
+      return true;
+    } else {
+      continue;
+    };
+  };
+  return false;
+};
+
 
 module.exports = {
-  // takeOrder,
-  // refundOrder,
-  // listItems,
-  // searchOrder
+  takeOrder,
+  refundOrder,
+  listItems,
+  searchOrder
 }
