4e76eb229d0e35db71eb68c3ae3ed05e9b67f098
Marceline Ball <marchettiball@gmail.com>
2021-02-07 18:01:02 -0700

    clean up unnecessary code

--- src/meal.js
+++ src/meal.js
@@ -16,13 +16,6 @@ function addIngredients(newIngredient, arrayForIgredients) {
   };
 };
 
-// function addIngredients(newIngredient, arrayForIgredients) {
-//   if (newIngredient !== arrayForIgredients[1]) {
-//     arrayForIgredients = arrayForIgredients.push(newIngredient);
-//   }
-// };
-// This only works for this particular instance of unique ingredients, I need to refactor for others
-
 function formatPrice(itemPrice) {
   return `$${itemPrice}`;
 };
@@ -37,23 +30,7 @@ function createRecipe(name, ingredients, menuItemType) {
     ingredients: ingredients,
     type: menuItemType,
   }
-
 }
-
-
-// var ingredients = [];
-// addIngredients("eggs", ingredients);
-// addIngredients("bacon", ingredients);
-//
-// var title = nameMenuItem("Eggs & Bacon");
-// var price = formatPrice("10.85")
-// var menuItem = createMenuItem(title, price, "breakfast");
-// var menuItemType = menuItem.type;
-//
-// var recipe = createRecipe(title, ingredients, menuItemType);
-// assert.equal(recipe.title, "Delicious Eggs & Bacon");
-// assert.deepEqual(recipe.ingredients, ["eggs", "bacon"]);
-// assert.equal(recipe.type, "breakfast");
 
 module.exports = {
   nameMenuItem,
